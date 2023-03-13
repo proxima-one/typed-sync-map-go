42246e5d83ba8fa504f0d6c6ff16386cd3ab0142
Matvey Maximov <manalog44@gmail.com>
2023-03-13 19:29:10 +0100

    added SyncMap description

--- pkg/map.go
+++ pkg/map.go
@@ -2,6 +2,28 @@ package syncMap
 
 import "sync"
 
+// SyncMap is like a Go map[K]V but is safe for concurrent use
+// by multiple goroutines without additional locking or coordination.
+// Loads, stores, and deletes run in amortized constant time.
+//
+// The SyncMap type is specialized. Most code should use a plain Go map instead,
+// with separate locking or coordination, for better type safety and to make it
+// easier to maintain other invariants along with the map content.
+//
+// The SyncMap type is optimized for two common use cases: (1) when the entry for a given
+// key is only ever written once but read many times, as in caches that only grow,
+// or (2) when multiple goroutines read, write, and overwrite entries for disjoint
+// sets of keys. In these two cases, use of a SyncMap may significantly reduce lock
+// contention compared to a Go map paired with a separate Mutex or RWMutex.
+//
+// The zero SyncMap is empty and ready for use. A SyncMap must not be copied after first use.
+//
+// In the terminology of the Go memory model, SyncMap arranges that a write operation
+// “synchronizes before” any read operation that observes the effect of the write, where
+// read and write operations are defined as follows.
+// Load, LoadAndDelete, LoadOrStore are read operations;
+// Delete, LoadAndDelete, and Store are write operations;
+// and LoadOrStore is a write operation when it returns loaded set to false.
 type SyncMap[K comparable, V any] struct {
 	sync.Map
 }
